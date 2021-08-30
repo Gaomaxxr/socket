e0bf6aed7e01c3a8aca53c410ed0d1d958dceb60
Gao <gao@gao.com>
2021-08-30 13:47:34 +0800

    在本地改一次

--- src/123.cpp
+++ src/123.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main(){
    printf("111\n");
-   printf("222\n");
-   printf("333\n");
+   printf("在本地改一次\n");
 
-   printf("555\n");
    int a=1,b=2;
    printf("a+b= %d\n",a+b);
    return 0;
